42beb1f1728b3cf6bd775597c17208fb0bbe4e46
hugo <hugo@hugo-golliet.dev>
2026-03-15 20:05:15 +0100

    chore: clear message from zod parsing error

--- src/auth/middlewares/validation.ts
+++ src/auth/middlewares/validation.ts
@@ -5,8 +5,8 @@ export function validateData(schema: z.ZodType<any>, field: 'body' | 'query') {
   return (req: Request, res: Response, next: NextFunction) => {
     const parsed = schema.safeParse(req[field]);
 
-    if (parsed.error) {
-      res.status(400).json({error: 'Invalid data: ' + parsed.error});
+    if (!parsed.success) {
+      res.status(400).json({error: 'Invalid data: ' + parsed.error.message});
     }
 
     next();
