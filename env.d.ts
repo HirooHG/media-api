560893b5159704030ff2e21b0daf984c39172404
hugo <hugo@hugo-golliet.dev>
2026-04-21 23:04:44 +0200

    feat: init keycloak

--- env.d.ts
+++ env.d.ts
@@ -2,11 +2,16 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      // minio
       MINIO_BUCKET: string;
       MINIO_ENDPOINT: string;
       MINIO_PORT: number;
       MINIO_ROOT_USER: string;
       MINIO_ROOT_PASSWORD: string;
+      // auth
+      KEYCLOAK_URL: string;
+      KEYCLOAK_REALM: string;
+      KEYCLOAK_CLIENT_ID: string;
     }
   }
 }
